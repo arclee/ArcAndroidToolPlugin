ca158e4f8fd9fb0b328bf0f201d4f2f4b598a52e
arclee <arclee0117@gmail.com>
2014-10-10 20:06:09 +0800

    Create

---
+++ Assets/ArcAndroidToolPlugin/Code/arcAndroidWebView.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arcAndroidWebView : MonoBehaviour {
+	
+	static public arcAndroidWebView Instance;
+	private static bool isAdInital = false;
+	private static AndroidJavaObject Jo = null;
+	
+	void Awake()
+	{
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+		}
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		DontDestroyOnLoad(gameObject);
+		Inital();
+	}
+
+	void Inital()
+	{
+		if (!isAdInital)
+		{
+#if UNITY_ANDROID
+			AndroidJavaClass player = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" );
+			AndroidJavaObject activity = player.GetStatic<AndroidJavaObject>("currentActivity");
+			Jo = new AndroidJavaObject("com.arc.arcandroidtool.ArcAndroidWebView", activity);
+			if (Jo != null)
+			{
+				isAdInital = true;
+			}
+#endif
+		}
+	}
+
+	public void ShowURL(string url)
+	{
+		if (Jo != null)
+		{		
+			Jo.Call("ShowURL", url);
+		}
+	}
+
+	public void CloseURL()
+	{
+		if (Jo != null)
+		{		
+			Jo.Call("CloseURL");
+		}
+	}
+
+	public void SetSizeScale(float x, float y)
+	{
+		if (Jo != null)
+		{	
+			Jo.Call("SetSizeScale", x, y);
+		}
+	}
+}
---
+++ Assets/ArcAndroidToolPlugin/Sample/Webviewtest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webviewtest : MonoBehaviour {
+
+	public string url;
+	ScreenOrientation lastOrit;
+	Vector2 scale = new Vector2(1.0f, 1.0f);
+	// Use this for initialization
+	void Start ()
+	{
+		lastOrit = Screen.orientation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{		
+		if (lastOrit != Screen.orientation)
+		{
+			lastOrit = Screen.orientation;
+			
+			arcAndroidWebView.Instance.SetSizeScale(scale.x, scale.y);
+		}
+	}
+
+	
+	void OnGUI ()
+	{
+
+		if (GUI.Button(new Rect (0, 100, 100, 100), "Size 0"))
+		{
+			scale.x = 1;
+			scale.y = 1;
+
+			arcAndroidWebView.Instance.SetSizeScale(scale.x, scale.y);
+			arcAndroidWebView.Instance.ShowURL(url);
+		}
+
+		
+		if (GUI.Button(new Rect (0, 200, 100, 100), "Size 1"))
+		{
+			scale.x = 0.9f;
+			scale.y = 0.9f;
+			arcAndroidWebView.Instance.SetSizeScale(scale.x, scale.y);
+			arcAndroidWebView.Instance.ShowURL(url);
+		}
+		
+		if (GUI.Button(new Rect (0, 300, 100, 100), "Size 2"))
+		{
+			scale.x = 0.5f;
+			scale.y = 0.5f;
+			arcAndroidWebView.Instance.SetSizeScale(scale.x, scale.y);
+			arcAndroidWebView.Instance.ShowURL(url);
+		}
+		
+		if (GUI.Button(new Rect (0, 400, 100, 100), "Size 2"))
+		{
+			scale.x = 2.0f;
+			scale.y = 2.0f;
+			arcAndroidWebView.Instance.SetSizeScale(scale.x, scale.y);
+			arcAndroidWebView.Instance.ShowURL(url);
+		}
+	}
+}
